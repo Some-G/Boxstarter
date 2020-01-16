ac7bfdd496169baed77a81c58e2d01ea02032fa2
Hugo Manrique <hugmanrique@gmail.com>
2020-01-16 18:32:46 +0100

    Add scripts to boxstarter.ps1

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -1,5 +1,10 @@
 Set-ExecutionPolicy AllSigned -Scope Process -Force
 
 & .\scripts\privacy.ps1
+& .\scripts\uninstallApps.ps1
 & .\scripts\uiPreferences.ps1
 & .\scripts\uxPreferences.ps1
+& .\scripts\windowsUpdate.ps1
+& .\scripts\installApps.ps1
+& .\scripts\wsl.ps1
+& .\scripts\renamePc.ps1
