6c17a03b3094282329ac348e6185fd5ce85c6b4f
Hugo Manrique <hugmanrique@gmail.com>
2020-01-18 20:38:10 +0100

    Make "Open Linux shell here" always show in context menu

--- scripts/configureApps.ps1
+++ scripts/configureApps.ps1
@@ -18,6 +18,13 @@ If (Test-Path HKCR:\Directory\shell\VSCode\command) {
   Write-Output ("Path: HKCR:\*\shell\VSCode\command Name: (default) Value: " + $template + '%1'')"')
 }
 
+# Make "Open Linux shell here" always show in context menu
+# This requires a registry ownership change described at
+# https://windowsreport.com/error-while-deleting-key-fix/
+#Remove-ItemProperty -Path HKCR:\Directory\Background\shell\WSL -Name "Extended"
+Write-Output "Due to registry ownership issues, you must manually remove the following registry value manually:"
+Write-Output "Path: HKCR:\Directory\Background\shell\WSL Name: Extended"
+
 Remove-PSDrive -Scope script -Name HKCR
 
 Write-Output "Applications successfully configured."
